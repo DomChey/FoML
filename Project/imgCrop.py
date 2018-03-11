7d1b8d3c8af7b41557f69b4648c62011a86e8fe7
Dominique Cheray <dcheray@posteo.net>
2018-03-12 00:06:47 +0100

    Finished code to cut image into pieces. Pieces are in LAB color space
    Function crop is to crop out an image out of a given image. function
    cutIntoPieces uses crop to cut a given image into several pieces and
    converts the pieces into LAB color space before finally returning a list
    containing all pieces cut out of the image.

---
+++ Project/imgCrop.py
@@ -0,0 +1,20 @@
+from skimage import io, color
+
+
+# crops piece out of given image
+def crop(infile, height, width):
+    im = io.imread(infile)
+    imgwidth, imgheight = im.shape[0], im.shape[1]
+    for i in range(imgheight//height):
+        for j in range(imgwidth//width):
+            yield im[(i*height):((i+1)*height), (j*width):((j+1)*width), :]
+
+
+# cuts given image into pieces and returns list containing the pieces
+def cutIntoPieces(infile, height, width):
+    start_num = 0
+    pieces = []
+    for k, piece in enumerate(crop(infile, height, width), start_num):
+        img = color.rgb2lab(piece)
+        pieces.append(img)
+    return pieces
