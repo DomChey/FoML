9f67ddd68442c7f48a3b1bc960b71565977b367b
Manuel <manuel.kraemer@stud.uni-heidelberg.de>
2018-03-22 12:55:33 +0100

    Added Evaluation Function Final.py will now save a results_log.txt where all important measures are saved for every image.

---
+++ Project/Evaluation.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def absoluteEval(source, solved):
+    # Returns the absolute score of a solved puzzle
+    # input: source list of all pieces, solved list with indices and according pieces
+    solvedList = []
+    row = []
+    col = []
+    for p in solved:
+        row.append(p[0])
+        col.append(p[1])
+
+    coldiff = max(col) - min(col)
+    for p in solved:
+        xpos = p[0]-min(row)
+        ypos = p[1]-min(col)
+        solvedList.append((xpos, ypos, p[2]))
+        
+    solvedList.sort(key=lambda p: p[0]*(coldiff+1)+p[1])
+    correct = 0
+    
+    for i,p in enumerate(source):
+        if np.array_equal(p, solvedList[i][2]):
+            correct = correct+1
+            
+    return correct/len(source)
+
--- Project/Final.py
+++ Project/Final.py
@@ -1,9 +1,11 @@
 from imgCrop import cutIntoPieces
 from Placer import placer, getImage, getShuffledImage
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from compatibility import *
 from FindStartingPiece import *
+from Evaluation import absoluteEval
 
 def clearAllMemoizedFunctions():
     dissmiliarity.clearMemo()
@@ -14,22 +16,34 @@ def clearAllMemoizedFunctions():
     hasFourBB.clearMemo()
 
 # Save all results from the whole dataset
-def solvePuzzle(i):
-
-    pieces = cutIntoPieces("imData/{}.png".format(i), 24, 24)
-    pieces = np.array(pieces)
+def solvePuzzle(i, log):
+    res = 80
+    sourcePieces = cutIntoPieces("imData/{}.png".format(i), res, res)
+    pieces = np.array(sourcePieces)
     np.random.shuffle(pieces)
     pieces = list(pieces)
     shuffledImage = getShuffledImage(pieces, 672, 504)
     plt.imsave("results/{}_shuffled.png".format(i), shuffledImage)
     
+    start_time = time.time()
     sort = placer(pieces)
+    elapsed_time = time.time() - start_time
+    
     finalImage = getImage(sort)
     plt.imsave("results/{}_solved.png".format(i), finalImage)
     clearAllMemoizedFunctions()
+    
+    absoluteScore = absoluteEval(sourcePieces, sort)
+    
+    log.write("Image {} with {} pieces\nElapsed time: {:.4f}s\nAbsolute Score: {:.3f}%\n\n"
+              .format(i, len(pieces), elapsed_time, absoluteScore*100))
 
 
-for i in range(1,21):
-    solvePuzzle(i)
 
-#solvePuzzle(4)
+log = open("results/results_log.txt", "w")
+
+for i in range(1,21):   
+    solvePuzzle(i, log)
+
+log.close()
+
