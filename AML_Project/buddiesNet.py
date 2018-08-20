0c06a0b60e1892df377dce11a9af466015d7722b
Dominique Cheray <dcheray@posteo.net>
2018-08-20 18:16:34 +0200

    Added first draft of neural net to determine DNN Buddies

---
+++ AML_Project/buddiesNet.py
@@ -0,0 +1,30 @@
+'''
+Neural Network to determine DNN Buddies
+
+@author: Dominique Cheray
+'''
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BuddiesNet(nn.Module):
+    """Implementation of the neural net for the DNN Buddies"""
+    def __ini__(self):
+        """Build the nertwork as described in the paper
+        https://arxiv.org/pdf/1711.08762.pdf"""
+        self.layers = nn.Sequential(
+            nn.Linear(336, 100),
+            nn.ReLU(True),
+            nn.Linear(100, 100),
+            nn.ReLU(True),
+            nn.Linear(100, 100),
+            nn.ReLU(True),
+            nn.Linear(100, 100),
+            nn.ReLU(True),
+            nn.Linear(100, 2))
+
+
+    def forward(self, x):
+        """Forward pass through BuddiesNet"""
+        out = self.layers(x)
+        return out
